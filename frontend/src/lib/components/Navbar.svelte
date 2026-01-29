434d16586ec8bb663fa9ab5884ba5fc9e742edad
00adrn <adrianvez11@gmail.com>
2026-01-29 17:48:34 -0500

    Mino navbar changes

--- frontend/src/lib/components/Navbar.svelte
+++ frontend/src/lib/components/Navbar.svelte
@@ -1,21 +1,23 @@
 <script>
-    import NavButton from "$lib/components/NavButton.svelte"
+  import NavButton from "$lib/components/NavButton.svelte";
 
-    let { data} = $props();
+  let { data } = $props();
 </script>
 
-<div class="w-screen flex flex-row items-center justify-between sticky top-0 pt-2 pb-2 pl-4 pr-4 ">
-    <div>
-        <p><a class="" href="/">symbalx</a></p>
-    </div>
+<div
+  class="w-full flex flex-row items-center justify-between sticky top-0 p-4 bg-gray-500/75"
+>
+  <div>
+    <p class="text-4xl font-extrabold"><a class="" href="/">symbalx</a></p>
+  </div>
 
-    <div class="flex flex-row gap-x-2 ">
-        {#if data.session}
-            <div><NavButton  text="Profile" href="/profile" /></div>
-        {:else}
-            <div><NavButton text="Login" href="/auth/login"/> </div>
-        {/if}
+  <div class="flex flex-row gap-x-2">
+    {#if data.session}
+      <div><NavButton text="Profile" href="/profile" /></div>
+    {:else}
+      <div><NavButton text="Login" href="/auth/login" /></div>
+    {/if}
 
-        <div><NavButton text="Home" href="/home" /></div>
-    </div>
+    <div><NavButton text="Home" href="/home" /></div>
+  </div>
 </div>
